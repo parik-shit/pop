e98fb952bae78258aac2aa1911a44a64767dea11
parik-shit <parikshitnasa@yahoo.in>
2024-07-31 22:32:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaTwitter, FaTelegramPlane } from 'react-icons/fa'; // Import social media icons
+import { GiPineapple } from 'react-icons/gi'; // Import pineapple icon
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-image-container">
+          <img
+            src={`${process.env.PUBLIC_URL}/goodpop.svg`}
+            className="App-logo"
+            alt="logo"
+          />
+          <div className="App-buttons">
+            <a
+              className="App-button"
+              href="https://twitter.com/yourprofile" // Replace with your Twitter URL
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter size={24} /> Twitter
+            </a>
+            <a
+              className="App-button"
+              href="https://t.me/yourprofile" // Replace with your Telegram URL
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTelegramPlane size={24} /> Telegram
+            </a>
+            <a
+              className="App-button"
+              href="https://yourcoinlink.com" // Replace with your Coin URL
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GiPineapple size={24} /> Pineapple
+            </a>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
